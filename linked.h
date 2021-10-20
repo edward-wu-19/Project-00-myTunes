7dcb9e0c27aba7e0902ffc0f39b593a8c46b5ae5
Sayeem2004 <MohammadSSKC@gmail.com>
2021-10-20 10:18:43 -0400

    10/20 Work

--- linked.h
+++ linked.h
@@ -15,8 +15,8 @@ int compare_node(struct song *s1, struct song *s2);
 struct song *insert_node_order(struct song *list, struct song *s);
 struct song *insert_new_node_order(struct song *list, char *name, char *artist);
 
-void print_list(struct song *front){}
-void print_song(struct song *song){}
+void print_list(struct song *front);
+void print_song(struct song *song);
 
 struct song *find_song(struct song *front, char *artist, char *song);
 struct song *find_first_song(struct song *front, char *artist);
--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include "linked.h"
+#include "library.h"
+
+void test_linked() {
+    printf("LINKED LIST TESTS\n");
+    printf("==================================================\n");
+
+    
+}
+
+void test_library() {
+
+}
 
 int main() {
-    
+    test_linked();
 }
