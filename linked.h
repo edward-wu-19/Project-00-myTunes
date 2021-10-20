604a1675878d698480435d565e44c88181c7b2d5
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-10-20 10:19:44 -0400

    k

--- linked.h
+++ linked.h
@@ -1,6 +1,7 @@
 #ifndef LINKED
 
 #define LINKED
+
 struct song;
 
 struct song *create_node(char* name, char *artist);
@@ -15,8 +16,8 @@ int compare_node(struct song *s1, struct song *s2);
 struct song *insert_node_order(struct song *list, struct song *s);
 struct song *insert_new_node_order(struct song *list, char *name, char *artist);
 
-void print_list(struct song *front){}
-void print_song(struct song *song){}
+void print_list(struct song *front);
+void print_song(struct song *song);
 
 struct song *find_song(struct song *front, char *artist, char *song);
 struct song *find_first_song(struct song *front, char *artist);
